631a37874eef7af0c311281a6ce7a749c44f6b50
nauvalachid <nauvalachid@gmail.com>
2023-03-30 16:05:51 +0700

    Add project files.

---
+++ QuickSort_117/QuickSort_117.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[20];				//array of integres to hold values
+int cmp_count = 0;			//number of comparison
+int mov_count = 0;			//number of data movements
+int n;
+
