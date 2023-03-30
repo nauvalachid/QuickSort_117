31756871a0e8a50675e2e45dfac6a70b2fb586a4
nauvalachid <nauvalachid@gmail.com>
2023-03-30 16:14:17 +0700

    Langkah Algoritma No.5

--- QuickSort_117/QuickSort_117.cpp
+++ QuickSort_117/QuickSort_117.cpp
@@ -56,4 +56,7 @@ void q_short(int low, int high)
 	j = high;							//Langkah Algoritma NO.4
 
 
-	
+	while (i <= j)						//Langkah Algoritma No.5
+	{
+		//search for an element less than or equal to pivot 
+		
